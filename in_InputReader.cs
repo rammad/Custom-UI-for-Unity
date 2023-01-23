2211679792129dad75a1f0c162f4d23898db6951
rammad <123408849+rammad@users.noreply.github.com>
2023-01-23 14:13:10 -0500

    Update in_InputReader.cs

--- in_InputReader.cs
+++ in_InputReader.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Works with the new Unity Input System package only!!! 
+// Be sure to have the Player Input set to 'Invoke Unity Events' and create corresponding Input Actions for each function ie. MouseMove, MouseClick, Select, ect.
+
+// Reads inputs and calls any associated functions within the UI system
+// Needs to be expanded to take any other inputs, ie. your gameplay inputs
+
 public class in_InputReader : MonoBehaviour
 {
     Mouse mouse;
     ui_Manager ui;
 
+    // Currently unused, remove unless there are issues down the road concerning switching between mouse and gamepad inputs
+    // Can be used as a condition when reading mouse inputs
     [SerializeField] bool readMouse = false;
 
-    private void Start()
+    // Finds the currently active Mouse and UI Manager
+    private void Start() 
     {
         mouse = Mouse.current;
         ui = FindObjectOfType<ui_Manager>();
     }
 
+    // Called every time the cursor's position changes, informs the UI Manager of the new position and checks for interactions with UI Elements
     public void onMouseMove(InputAction.CallbackContext ctx)
     {
         Vector3 pos = mouse.position.ReadValue();
@@ -23,6 +33,7 @@ public class in_InputReader : MonoBehaviour
         ui.hoverCheck();
     }
 
+    // Attempts to press whichever UI Element is currently marked as Selected
     public void onMouseClick(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
@@ -31,6 +42,8 @@ public class in_InputReader : MonoBehaviour
         }
     }
 
+    // Attempts to press whichever UI Element is currently marked as Selected
+    // Possibly redundant, but for now separates selection with the mouse and selection with a gamepad or keyboard
     public void onSelect(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
@@ -39,6 +52,7 @@ public class in_InputReader : MonoBehaviour
         }
     }
 
+    // Switches which UI Element is currently Selected when using a gamepad or keyboard to navigate
     public void onUiMove(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
